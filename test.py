7565d85495c057b3418204614a6cf2bd38ff1ac4
Pertsev Roman <pertsev.roman@gmail.com>
2016-05-25 13:48:31 +0300

    всяки лямбды

--- test.py
+++ test.py
@@ -2,13 +2,19 @@ from libbp import World
 
 class Test:
     def fn(self):
-        print('inc')
+        print('one')
 
 if __name__ == "__main__":
     w = World("Income Software")
 
     n = Test()
 
+    def r():
+        print('two')
+
     print(w.greet())
     print(w.simplesCount(10000))
+
     w.function(n.fn)
+    w.function(r)
+    w.function(lambda : print('three'))
